7b64c4a25183f71451fcc0ae918c03894446f74f
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-16 22:31:46 +0700

    text-widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     Container(
-      color: Colors.blue
-    )
+      color: Colors.blue,
+      child: Text("Haloo from flutter", textDirection: TextDirection.ltr, style: TextStyle(fontSize:32.0, color: Colors.white))
+    ) // container
   );
 }
