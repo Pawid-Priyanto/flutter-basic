b9d91f505b1ba62dbc98e49a530035267ad072ca
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-17 12:06:14 +0700

    stateless-widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Container(
+   MyStatelesswidget()
+  );
+}
+
+class MyStatelesswidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
       color: Colors.blue,
       child: Center(
         child: Text("Haloo from flutter", textDirection: TextDirection.ltr, style: TextStyle(fontSize:32.0, color: Colors.white))
       ) // center
-    ) // container
-  );
+    ); // container
+  }
 }
