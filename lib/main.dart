0ea8eda66eda684713e2f162856556fa1976ffe6
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-19 21:16:57 +0700

    material-appbar

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-   MyStatefullWidget()
+    MaterialApp(
+      home:  MyStatefullWidget()
+    )
   );
 }
 class MyStatefullWidget extends StatefulWidget {
@@ -18,71 +20,22 @@ class _MyStatefullWidgetState extends State<MyStatefullWidget> {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
-      home: Padding(
-      padding: EdgeInsets.only(top: 150.0, left: 70.0, right: 60.0),
-      child: Column (
-        mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-       Card( 
-         child: Padding(
-           padding: EdgeInsets.all(20.0),
-         child: Row(
-           textDirection: TextDirection.ltr,
-           children: <Widget>[
-             IconButton(
-               icon: Icon(Icons.person, textDirection: TextDirection.ltr,),
-               onPressed: (){
-                 print("ini icon");
-                }
-               ),
-               Expanded(
-                 child: Text("Child 2", textDirection: TextDirection.ltr,)),
-       
-              IconButton(
-               icon: Icon(Icons.person, textDirection: TextDirection.ltr,),
-               onPressed: (){
-                 print("ini icon");
-                }
-               ),
-           ],
-           )
-          )
-       ),
-       Padding(
-         padding: EdgeInsets.only(top: 8.0),
-       child: Card( 
-         child: Padding(
-           padding: EdgeInsets.all(20.0),
-         child: Row(
-           textDirection: TextDirection.ltr,
-           children: <Widget>[
-             IconButton(
-               icon: Icon(Icons.person, textDirection: TextDirection.ltr,),
-               onPressed: (){
-                 print("ini icon");
-                }
-               ),
-             Expanded(
-               child: Text("Child 2 2", textDirection: TextDirection.ltr, )),  
-             
-              IconButton(
-               icon: Icon(Icons.person, textDirection: TextDirection.ltr,),
-               onPressed: (){
-                 print("ini icon");
-                }
-               ),
-           ],
-           )
-          )
-          )
-       ),
-       Image(
-         image: AssetImage("images/hilda.png"),
-         )
-      ],
-     )
-    )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Begginer", textDirection: TextDirection.ltr,),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.chat),
+            onPressed: (){
+              print("This is icon");
+            }),
+          IconButton(
+              icon: Icon(Icons.add_circle),
+              onPressed: (){
+                print("This is icon");
+              })    
+        ],
+      ),
     );
   }
 }
