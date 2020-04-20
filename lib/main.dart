1c6763a98be4f92e580c013df7b0fd779005c945
Pawid-prianto27 <pawitanto27@gmail.com>
2020-04-20 11:39:26 +0700

    navigation-drawer

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,87 @@ class _MyStatefullWidgetState extends State<MyStatefullWidget> {
               })    
         ],
       ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Image(
+                  image: AssetImage("images/inlove.JPG"),
+                  ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0, left: 16.0),
+                  child: CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage(
+                      "images/me.JPG"
+                    )
+                  ),
+                  ),
+                Padding(
+                  padding: EdgeInsets.only(top: 120.0, left: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("First Time", style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 12.0, color: Colors.white)),
+                      Text("My Flutter Tutorial", style: TextStyle(color: Colors.white)),  
+                    ]
+                  )
+                  )  
+              ]
+            ),
+            ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.add_circle),
+                  title: Text("Tutorial Flutter"),
+                  onTap: (){
+
+                  },
+                )
+              ],
+            ),
+             ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.add_circle),
+                  title: Text("Tutorial React Native"),
+                  onTap: (){
+
+                  },
+                )
+              ],
+            ),
+             ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.add_circle),
+                  title: Text("Tutorial React JS"),
+                  onTap: (){
+
+                  },
+                )
+              ],
+            ),
+             ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.add_circle),
+                  title: Text("Tutorial Kotlin"),
+                  onTap: (){
+
+                  },
+                )
+              ],
+            )      
+          ],
+          ),
+      ),
     );
   }
 }
